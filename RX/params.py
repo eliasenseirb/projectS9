db2ae9cdb83a7618afbca9f8b21a585b8cff3333
Alexandre Valade <alvalade@bordeaux-inp.fr>
2023-02-01 21:19:21 +0100

    Updated params for radios

--- RX/params.py
+++ RX/params.py
@@ -3,23 +3,25 @@ import math
 class params:
 
     def __init__(self, ebn0):
-        self.K = 512
-        self.N = 1024
-        self.R = 1/2
-        self.ebn0 = ebn0
-        self.frozen_bits = []
-        self.noise = None # temporaire
-        self.sec_sz = 0
-        self.window_size = 20
-        self.Fs = 1e6
-        self.Fc = 868e6
-        self.n_frames =1
-        self.MODCOD = "QPSK-S_8/9"
-        self.p = 16200
+        """Common parameters for Bob and Eve"""
+        #self.K = 512
+        self.N = 2048              # Polar encoder output size
+        self.R = self.K/self.N     # Rate of the code
+        self.ebn0 = ebn0           # SNR
+        self.frozen_bits = []   
+        self.noise = None
+        self.sec_sz = 0            # number of secrecy bits
+        self.window_size = 20      # unused
+        self.Fs = 1e6              # Sampling frequency
+        self.Fc = 868e6            # carrier frequency
+        self.n_frames =1           # number of frames
+        self.MODCOD = "QPSK-S_8/9" # modulation
+        self.p = 16200             # padding size 
 
 class Bob(params):
     def __init__(self, ebn0):
-        self.K = 900
+        """Specific parameters for Bob"""
+        self.K = 1900
         super().__init__(ebn0)
         self.esn0 = self.ebn0 + 10*math.log10(self.K/self.N)
         self.sigma = 1/(math.sqrt(2)*10**(self.esn0/20))
@@ -27,7 +29,8 @@ class Bob(params):
 
 class Eve(params):
     def __init__(self, ebn0):
-        self.K = 500
+        """Specific parameters for Eve"""
+        self.K = 1500
         super().__init__(ebn0)
         self.ebn0 -= 3
         self.esn0 = self.ebn0 + 10*math.log10(self.K/self.N)
--- TX/params.py
+++ TX/params.py
@@ -3,22 +3,24 @@ import math
 class params:
 
     def __init__(self, ebn0):
+        """Common parameters for Bob and Eve"""
         #self.K = 512
-        self.N = 2048
-        self.R = 1/2
-        self.ebn0 = ebn0
+        self.N = 2048              # Polar encoder output size
+        self.R = self.K/self.N     # Rate of the code
+        self.ebn0 = ebn0           # SNR
         self.frozen_bits = []
-        self.noise = None # temporaire
-        self.sec_sz = 0
-        self.window_size = 20
-        self.Fs = 1e6
-        self.Fc = 2.45e9
-        self.n_frames =1
-        self.MODCOD = "QPSK-S_8/9"
-        self.p = 16200
+        self.noise = None     
+        self.sec_sz = 0            # number of secrecy bits
+        self.window_size = 20      # unused
+        self.Fs = 1e6              # Sampling frequency
+        self.Fc = 2.45e9           # carrier frequency
+        self.n_frames =1           # number of frames
+        self.MODCOD = "QPSK-S_8/9" # modulation
+        self.p = 16200             # padding size 
 
 class Bob(params):
     def __init__(self, ebn0):
+        """Specific parameters for Bob"""
         self.K=1900
         super().__init__(ebn0)
         self.esn0 = self.ebn0 + 10*math.log10(self.K/self.N)
@@ -27,6 +29,7 @@ class Bob(params):
 
 class Eve(params):
     def __init__(self, ebn0):
+        """Specific parameters for Eve"""
         self.K=1500
         super().__init__(ebn0)
         self.ebn0 -= 3
